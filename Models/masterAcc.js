d026808bac5e2702ad8f6a21dd6310591ea71011
ayush-code-drops <ayushmangal121212@gmail.com>
2022-04-25 21:45:23 +0530

    post routes created

--- Models/masterAcc.js
+++ Models/masterAcc.js
@@ -5,6 +5,7 @@ let Schema = mongoose.Schema;
 let masterAc = new Schema(
     {
         "userId": { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+        "branchId": { type: mongoose.Schema.Types.ObjectId, ref: 'branches' },
         "balance": {type:Number, required: true},
         "createdAt": { type: Date, default: Date.now },
         "updatedAt": { type: Date, default: Date.now} 
--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const app = express();
 const port = 9000;
-const connectToDb=require('./mongoDb')
+const connectToDb = require('./mongoDb')
+const bodyParser = require('body-parser');
+const savingAccModel=require('./Models/savingAcc')
+const masterAccModel=require('./Models/masterAcc')
+const fixedAccModel=require('./Models/fixedAcc')
+const userModel=require('./Models/user')
+const branchModel = require('./Models/branch');
+
+app.use(bodyParser.json())
 app.listen(port, () => {
    new connectToDb();
     console.log('Server is running on port 9000');
 })
+//to create Saving Acount
 
+app.post('/savingAcc', async (req, res, next) => {
+    try {
+        let accDetials = req.body;
+        let response = await savingAccModel.insertOne(accDetials);
+        res.json(req.body);   
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+   
+})
+
+//to create Fixed Account
+app.post('/fixedAcc', async (req, res, next) => {
+    try {
+        let accDetials = req.body;
+        let response = await fixedAccModel.insertOne(accDetials);
+        res.json(req.body);   
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+   
+})
 
